9a8f68a06f1f6d29f99b9087cdc1abe7b8aef678
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-08-01 00:36:47 +0500

    bug fixing

--- app/Models/PaymentRequest.php
+++ app/Models/PaymentRequest.php
@@ -9,7 +9,7 @@ class PaymentRequest extends Model
 {
     use HasFactory;
 
-    protected $table = 'Payment_Requests';
+    protected $table = 'payment_requests';
 
     protected $fillable = [
         'Full_Name', 'Email', 'Phone_No', 'TRC20_ID', 'Recharge_Amount', 'image','user_id'
